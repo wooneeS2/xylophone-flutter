da01039221e5e620525be000eaa340e5831c40a5
KIM WONHEE <soyoung0007@naver.com>
2021-02-11 22:32:52 +0900

    .

---
+++ xloyphone_flutter_app/android/app/src/main/kotlin/wonhee/example/xloyphone_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package wonhee.example.xloyphone_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xloyphone_flutter_app/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum){
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
+  Expanded buildKey({Color color, int SoundNumber}) {
+    return Expanded(
+      child:FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(SoundNumber);
+        },
+      ),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget> [
+                buildKey(color:Colors.red, SoundNumber : 1),
+                buildKey(color:Colors.orange, SoundNumber : 2),
+                buildKey(color:Colors.yellow, SoundNumber : 3),
+                buildKey(color:Colors.green, SoundNumber : 4),
+                buildKey(color:Colors.blue, SoundNumber : 5),
+                buildKey(color:Colors.blue[900], SoundNumber : 6),
+                buildKey(color:Colors.purple, SoundNumber : 7),
+
+
+
+
+
+              ],
+
+            )
+
+
+        ),
+      ),
+    );
+  }
+}
